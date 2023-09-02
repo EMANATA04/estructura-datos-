6c66ae1efcb4241c7a1fb37e5c96cfd7ca918888
EMANATA04 <141505065+EMANATA04@users.noreply.github.com>
2023-09-02 14:35:01 +0000

    tarea recursividad

---
+++ clase4/ejercicio1.py
@@ -0,0 +1,29 @@
+import random
+
+
+class Nodo:
+    dato = None
+    apuntador = None
+    def __init__(self, dato, apuntador):
+        self.dato = dato 
+        self.apuntador = apuntador
+
+    def __str__(self):  
+        return f"{self.dato}"
+    
+def lista_nodos(n):
+    if n == 0:
+        return [n]
+    else:
+        return lista_nodos
+    
+def lista_random(n):
+    lista = [0] * n
+    for i in range (n):
+        lista[i] = random.random()
+    return lista
+
+n = input("Digite el número de nodos que desea crear: ")
+print(lista_nodos(4))
+aleatorios = lista_random(n)
+print(aleatorios)
